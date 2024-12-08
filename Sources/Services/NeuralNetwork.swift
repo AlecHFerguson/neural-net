3df5f8f970220afe1d1cc3820c428bd15111b6d0
Alec Ferguson <alec.ferguson@gmail.com>
2024-12-07 19:52:04 -0800

    Initial commit

---
+++ Sources/Services/NeuralNetwork.swift
@@ -0,0 +1,11 @@
+class NeuralNetwork {
+    let inputSize, layerSize, numberOfLayers: Int
+    
+    init(inputSize: Int, layerSize: Int, numberOfLayers: Int) {
+        self.inputSize = inputSize
+        self.layerSize = layerSize
+        self.numberOfLayers = numberOfLayers
+    }
+    
+    
+}
